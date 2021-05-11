50d12896906529d6571c216bd39b03325db3ba97
dılo sürücü <berxudar@gmail.com>
2021-05-11 10:16:01 +0300

    NODE_ENV had been added  also json determined as response

--- middleware/token.js
+++ middleware/token.js
@@ -3,6 +3,7 @@ const token = function (app) {
 
     app.use((req, res, next) => {
         if (!req.query.token) {
+            res.status(401);
             return res.json({status: false, message: "not authorized token missing"});
         }
         if (verifyToken(req.query.token)) {
